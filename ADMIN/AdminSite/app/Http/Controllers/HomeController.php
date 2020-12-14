53643e592670bff93e32980c499903c2ad415cfb
Monirul Islam Akand <45770524+monirulakand@users.noreply.github.com>
2020-12-15 01:53:15 +0600

    Admin Panel Dashboard Summary Complete
    Admin Panel Dashboard Summary Complete

--- ADMIN/AdminSite/app/Http/Controllers/HomeController.php
+++ ADMIN/AdminSite/app/Http/Controllers/HomeController.php
@@ -17,10 +17,26 @@ class HomeController extends Controller
 
         function HomeIndex(){
 
-          
+            $TotalVisitor=VisitorModel::count();
+            $TotalCourse=MoreSeriesModel::count();
+            $TotalStudent=StudentListModel::count();
+            $TotalLogin=LoginSuccessModel::count();
+            $TotalTeacher=TeacherModel::count();
+            $TotalNotice=NoticeModel::count();
+            $TotalBlog=BlogModel::count();
+            $TotalContact=ContactModel::count();
+            $TotalReview=ReviewModel::count();
 
             return view('Home',[
-
+                  'TotalVisitor'=>$TotalVisitor,
+                  'TotalCourse'=>$TotalCourse,
+                  'TotalStudent'=>$TotalStudent,
+                  'TotalLogin'=>$TotalLogin,
+                  'TotalTeacher'=>$TotalTeacher,
+                  'TotalNotice'=>$TotalNotice,
+                  'TotalBlog'=>$TotalBlog,
+                  'TotalContact'=>$TotalContact,
+                  'TotalReview'=>$TotalReview,
             ]);
         }
 
--- ADMIN/AdminSite/resources/views/Home.blade.php
+++ ADMIN/AdminSite/resources/views/Home.blade.php
@@ -6,7 +6,89 @@
 <div class="container">
 	<div class="row">
 
+        <div class="col-md-3 py-2">
+        	<div class="card card-color">
+        		<div class="card-body">
+        			<h4 class="count-card-title">{{$TotalVisitor}}</h4>
+					<h5 class="count-card-text">Total Visitor</h5>
+        		</div>
+        	</div>
+        </div>
 
+        <div class="col-md-3 py-2">
+            <div class="card card-color">
+                <div class="card-body">
+                    <h4 class="count-card-title">{{$TotalCourse}}</h4>
+                    <h5 class="count-card-text">Total Course</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 py-2">
+            <div class="card card-color">
+                <div class="card-body">
+                    <h4 class="count-card-title">{{$TotalStudent}}</h4>
+                    <h5 class="count-card-text">Total Student</h5>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-md-3 py-2">
+            <div class="card card-color">
+                <div class="card-body">
+                    <h4 class="count-card-title">{{$TotalLogin}}</h4>
+                    <h5 class="count-card-text">Total Login</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 py-2">
+            <div class="card card-color">
+                <div class="card-body">
+                    <h4 class="count-card-title">{{$TotalTeacher}}</h4>
+                    <h5 class="count-card-text">Total Teacher</h5>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="col-md-3 py-2">
+            <div class="card card-color">
+                <div class="card-body">
+                    <h4 class="count-card-title">{{$TotalNotice}}</h4>
+                    <h5 class="count-card-text">Total Notice</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 py-2">
+            <div class="card card-color">
+                <div class="card-body">
+                    <h4 class="count-card-title">{{$TotalBlog}}</h4>
+                    <h5 class="count-card-text">Total Blog</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 py-2">
+            <div class="card card-color">
+                <div class="card-body">
+                    <h4 class="count-card-title">{{$TotalContact}}</h4>
+                    <h5 class="count-card-text">Total Contact</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3 py-2">
+            <div class="card card-color">
+                <div class="card-body">
+                    <h4 class="count-card-title">{{$TotalReview}}</h4>
+                    <h5 class="count-card-text">Total Review</h5>
+                </div>
+            </div>
+        </div>
 
 	</div>
 </div>
